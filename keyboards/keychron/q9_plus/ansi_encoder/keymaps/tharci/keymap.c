d04f1b0c1ba76a3e2f91ef983868584f5a672d89
Marcell Tamás Tóth <tmarcellt.2000@gmail.com>
2023-11-15 17:47:08 +0100

    Minor keymap changes

--- keyboards/keychron/q9_plus/ansi_encoder/keymaps/tharci/keymap.c
+++ keyboards/keychron/q9_plus/ansi_encoder/keymaps/tharci/keymap.c
@@ -17,6 +17,7 @@
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 
+
 // TODO: try to fix this
 // only needed for intellisense to work
 #ifndef MATRIX_COLS
@@ -34,6 +35,7 @@
 #ifndef ENCODERS_PAD_B
 #    define ENCODERS_PAD_B { B5 }
 #endif // ENCODERS_PAD_B
+
 
 enum layers {
     LY_BASE,
@@ -163,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LY_FNC] = LAYOUT_54_ansi(
         KC_ESC,   _______,  KC_CUT, KC_COPY, KC_PASTE, KC_SELECT, _______, _______, KC_UP, _______, _______,  KC_HOME,     KC_END,  KC_DEL,          KC_END,
-        _______,  _______,  KC_SAVE,  KC_UNDO, KC_REDO, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,  _______,               _______,          _______,
+        _______,  _______,  KC_SAVE,  KC_UNDO, KC_REDO, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP,  KC_PGDN,               _______,          _______,
         _______,            _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,               _______, _______,
         _______, _______,  _______,  KC_LCTL,          KC_TABLOOP,                   _______,          _______,  _______,     _______,  _______, _______, _______),
 
@@ -174,10 +176,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______,  _______,          _______,                   _______,          _______,  _______,     _______,  _______, _______, _______),
 
     [LY_GAMING] = LAYOUT_54_ansi(
-        _______,   _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,          _______,
+        _______,   _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,          RGB_TOG,
         _______,  _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,               _______,          _______,
         _______,            _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,               _______, _______,
-        _______, _______,  _______,  _______,          _______,                   _______,          _______,  _______,     _______,  _______, _______, _______),
+        _______, _______,  _______,  MO(LY_FN_GAMING),          _______,                   _______,          _______,  _______,     _______,  _______, _______, _______),
 
     [LY_FN_GAMING] = LAYOUT_54_ansi(
         KC_ESC, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,      KC_F12,   _______,          _______,
@@ -215,7 +217,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [LY_BASE] = {ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)},
     [LY_FNC]  = {ENCODER_CCW_CW(KC_UP, KC_DOWN)},
     [LY_GAMING]  = {ENCODER_CCW_CW(_______, _______)},
-    [LY_FN_GAMING]  = {ENCODER_CCW_CW(_______, _______)},
+    [LY_FN_GAMING]  = {ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)},
     [LY_FN_SPACE]  = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [LY_FN1]      = {ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)}
 };
